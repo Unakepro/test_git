b7f75414eec2b60855743dcce165e50679e5f904
1 <1>
2020-07-28 16:57:42 +0300

    imp commit

--- git.py
+++ git.py
@@ -1,6 +1,12 @@
+# master - actual code
+# stage - test new code
+# ref - task f  or programing
+
+
 def get_random(n):
     import random
     random.randint(1, n)
+
 
 if __name__ == "__main__":
     print("Hello")
